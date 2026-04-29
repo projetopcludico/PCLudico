383f233d17f5ed9c2f5eb8c36b646af08840a0c2
AndreRicardoSc <andrenarutto7@gmail.com>
2026-04-28 21:20:19 -0300

    FIX: Adding linear-gradient in HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="h-full w-full">
-    <div class="h-screen bg-purple dark:bg-purple-dark px-20 py-10">
+    <div class="h-screen bg-linear-to-b from-white to-purple dark:from-slate-950 dark:to-purple-dark px-20 py-10">
       <div
         class="flex flex-col justify-center items-center w-full h-full bg-white rounded-4xl dark:bg-slate-950 dark:text-white"
       >
