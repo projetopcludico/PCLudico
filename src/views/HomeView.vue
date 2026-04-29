616080d684a1c6c71d82204e4c22de494b9a6ce7
AndreRicardoSc <andrenarutto7@gmail.com>
2026-04-29 07:56:10 -0300

    FEAT: Adding waves and upload video

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,22 @@
+<script setup>
+import CellingWaves from '@/components/decorators/CellingWaves.vue';
+</script>
 <template>
   <section class="h-full w-full">
-    <div class="h-100 bg-linear-to-b from-white to-purple px-5 py-2 dark:from-slate-950 dark:to-purple-dark md:h-screen md:px-20 md:py-10">
-      <div
-        class="flex flex-col justify-center items-center w-full h-full bg-white rounded-4xl dark:bg-slate-950 dark:text-white"
+    <div
+      class="flex justify-center h-100 bg-linear-to-b from-white to-purple px-5 py-2 dark:from-slate-950 dark:to-purple-dark md:h-screen md:px-20 md:py-10"
+    >
+      <video
+        controls
+        src="/videos/tutorial.mp4"
+        class="flex flex-col justify-center h-full items-center shadow-xl bg-white rounded-4xl dark:bg-slate-950 dark:text-white"
       >
         <span class="mdi mdi-play text-6xl"></span>
-      </div>
+      </video>
+      
     </div>
-    <div class=""></div>
+    <div class="">
+      <CellingWaves/>
+    </div>
   </section>
 </template>
