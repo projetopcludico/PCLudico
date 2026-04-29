f9dc9576351b2e07eb52462dad05b6160ee69b72
AndreRicardoSc <andrenarutto7@gmail.com>
2026-04-28 21:36:05 -0300

    FIX: Adjusting responsiveness

--- src/components/layouts/AppHeader.vue
+++ src/components/layouts/AppHeader.vue
@@ -9,15 +9,15 @@ const { increment, decrement } = useFontSize()
   >
     <h1 class="flex items-center gap-2">
       <img class="w-8 h-8" src="/imgs/logo.svg" alt="logo" />
-      <p class="text-2xl font-semibold font-serif">TRAMA</p>
+      <p class="font-semibold font-serif text-xl md:text-2xl">TRAMA</p>
     </h1>
     <ul class="flex gap-4 items-center">
-      <li class="flex gap-2">
+      <li class="gap-2 hidden md:flex">
         <AppButton text="A+" @on-click="increment" />
         <AppButton text="A-" @on-click="decrement" />
       </li>
       <li>
-        <button class="border-2 rounded-full w-10 h-10 cursor-pointer transition-all duration-200 hover:scale-98 active:scale-97 hover:opacity-80">
+        <button class="border-2 rounded-full w-8 h-8  cursor-pointer transition-all duration-200 hover:scale-98 active:scale-97 hover:opacity-80 md:w-10 md:h-10">
           <span class="mdi mdi-human text-xl"></span>
         </button>
       </li>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="h-full w-full">
-    <div class="h-screen bg-linear-to-b from-white to-purple dark:from-slate-950 dark:to-purple-dark px-20 py-10">
+    <div class="h-100 bg-linear-to-b from-white to-purple px-5 py-2 dark:from-slate-950 dark:to-purple-dark md:h-screen md:px-20 md:py-10">
       <div
         class="flex flex-col justify-center items-center w-full h-full bg-white rounded-4xl dark:bg-slate-950 dark:text-white"
       >
