38d7e92235567f60e223b983a114abf11a4472e0
AndreRicardoSc <andrenarutto7@gmail.com>
2026-05-27 15:14:17 -0300

    FIX: Removing watch from achievementStore

--- src/stores/achievements.js
+++ src/stores/achievements.js
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import { ref, watch } from 'vue'
+import { ref } from 'vue'
 import achievementsData from '@/data/achievements.json'
 
 const STORAGE_KEY = 'pcludico-achievements'
