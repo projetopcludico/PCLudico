782e0496c7ebf4a9d32e10d56e17043d490c90ae
AndreRicardoSc <andrenarutto7@gmail.com>
2026-05-27 16:07:05 -0300

    FIX: Adding cursor-pointer in AdvenureCard

--- src/components/cards/AdventureCard.vue
+++ src/components/cards/AdventureCard.vue
@@ -9,13 +9,20 @@ const { title, image, description } = defineProps({
 </script>
 
 <template>
-  <div class="group p-0.5 rounded-4xl bg-linear-to-b from-blue via-red to-orange transition-all duration-200 hover:-translate-y-2">
+  <div
+    class="group p-0.5 rounded-4xl bg-linear-to-b from-blue via-red to-orange transition-all duration-200 hover:-translate-y-2"
+  >
     <button
-      class="relative flex flex-col justify-between p-8 bg-white dark:bg-slate-900 rounded-4xl w-full h-full"
+      class="relative flex flex-col justify-between p-8 bg-white dark:bg-slate-900 rounded-4xl w-full h-full cursor-pointer"
       @click="emit('click')"
     >
       <div class="flex justify-center">
-        <img v-if="image" :src="image" alt="" class="group-hover:scale-110 group-hover:-translate-y-2 transition-all duration-200 xl:-mt-40" />
+        <img
+          v-if="image"
+          :src="image"
+          alt=""
+          class="group-hover:scale-110 group-hover:-translate-y-2 transition-all duration-200 xl:-mt-40"
+        />
       </div>
 
       <div class="flex flex-col gap-2">
