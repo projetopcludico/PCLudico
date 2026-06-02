cfa114c8bbc10a14b30bb7a86f3f02068232b144
AndreRicardoSc <andrenarutto7@gmail.com>
2026-06-02 12:59:02 -0300

    FEAT: Randomizing sortObjects

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -1,22 +1,30 @@
-function sortObjects(quantidade, arrayObj){
-    if(quantidade > arrayObj.length){
-        throw new Error('Quantidade não pode ser maior do que o tamanho do array');
+function sortObjects(quantidade, arrayObj) {
+  if (quantidade > arrayObj.length) {
+    throw new Error('Quantidade não pode ser maior do que o tamanho do array')
+  }
+
+  const uniqueIds = new Set(arrayObj.map((obj) => obj.id))
+  if (uniqueIds.size <= 1 && quantidade > 1) {
+    throw new Error('O array só possui 1 item único')
+  }
+
+  while (true) {
+    const selecionados = []
+
+    for (let i = 0; i < quantidade; i++) {
+      const randomIndex = Math.floor(Math.random() * arrayObj.length)
+      selecionados.push(arrayObj[randomIndex])
     }
 
-    const selecionados = [];
-    while(selecionados.length < quantidade) {
-        const randomIndex = Math.floor(Math.random() * arrayObj.length);
-        const drawn = arrayObj[randomIndex];
+    if (quantidade <= 1) return selecionados
 
-        if(selecionados.length == (quantidade -1)){
-            if(selecionados.find(obj => obj.id == drawn.id)){
-                continue;
-            }
-        }        
-        selecionados.push(drawn)
+    const firstId = selecionados[0].id
+    const allTheSame = selecionados.every((obj) => obj.id === firstId)
+
+    if (!allTheSame) {
+      return selecionados
     }
-
-    return selecionados;
+  }
 }
 
-export default sortObjects;
+export default sortObjects
